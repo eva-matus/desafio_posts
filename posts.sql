27daeb6a127d7484672649afc57541b7fbf578e5
eva-matus <eva.matus@hotmail.com>
2020-09-16 18:41:12 -0300

    Create post table posts

--- posts.sql
+++ posts.sql
@@ -7,3 +7,22 @@ Parte 1
 Instrucciones
 Parte 1
 1. Crear Base de datos posts (1 punto)
+
+postgres=# \c eva
+You are now connected to database "eva" as user "postgres".
+eva=# CREATE DATABASE posts;
+CREATE DATABASE
+
+2. Crear tabla post, con los atributos id, nombre de usuario, fecha de creación, contenido,
+descripción (1 punto)
+
+eva=# CREATE TABLE posts(
+eva(# id SERIAL,
+eva(# usuario VARCHAR(25),
+eva(# fecha_de_creacion DATE,
+eva(# contenido VARCHAR,
+eva(# descripcion VARCHAR,
+eva(# PRIMARY KEY (id));
+CREATE TABLE
+
+
